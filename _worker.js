93ae4d7f04ef5d2b92fbc595ce7cd5cd6584ffbf
kassul <89822501+kassul@users.noreply.github.com>
2022-05-26 00:59:41 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'teszv.herokuapp.com'
+      url.hostname = 'twotes-guiewpewxdwp.koyeb.app'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
